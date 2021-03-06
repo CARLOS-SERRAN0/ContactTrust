4d3d32cae9cbbeace0393b938e2f9d6d0ecb6cb8
No Class Post <noclasspost@Carloss-iMac.local>
2021-03-06 12:49:51 -0800

    register route and hash password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -17,13 +18,38 @@ router.post(
       "Please enter a password with 6 or more charaters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("passed");
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+      // creates instance of new user not saved yet
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User saved");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
